7cfad2e79002db8921da898dc8f7e66048017363
Jake Anderson <jakeandersonwebdevelopment@gmail.com>
2021-11-01 15:03:45 -0500

    db
    db

--- backend/database.js
+++ backend/database.js
@@ -2,14 +2,14 @@ var mysql = require('mysql');
 
 
 exports.con = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "password"
+	username : "doadmin",
+	password : "L8xd9QdhCzZmRfvQ",
+	host : "db-mysql-nyc3-28502-do-user-8887882-0.b.db.ondigitalocean.com",
+	port : "25060",
+	database : "maindatabase"
 });
 
 exports.con.connect(function(err) {
 	if (err) throw err;
 	console.log("Connected!" );
 })
-
-
